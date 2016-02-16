7ef31ee9df92c758093d8285e79460dae911e836
Branko Simic <branko.simic.sarajevo@gmail.com>
2016-02-16 23:45:58 +0100

    Added react library and added sample .jsx file and rendered it.

---
+++ app.js
@@ -0,0 +1,5 @@
+﻿var React = require('react');
+var Books = require('./views/index.jsx');
+
+var books = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
+React.render(<Books books={ books } />, document.getElementById('container'));
---
+++ server.js
@@ -0,0 +1,41 @@
+﻿var http = require('http');
+var React = require('react');
+var jsx = require('node-jsx');  
+
+var port = process.env.port || 1337;
+
+jsx.install();
+var Books = require('./Views/index.jsx');
+
+http.createServer(function (req, res) {
+    
+    var books = [{
+            title: 'Professional Node.js',
+            read: false
+        }, {
+            title: 'Node.js Patterns',
+            read: false
+        }];
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(React.renderToStaticMarkup(
+        React.DOM.body(
+            null,
+      React.DOM.div({
+                id: 'container',
+                dangerouslySetInnerHTML: {
+                    __html: React.renderToString(React.createElement(Books, {
+                        books: books
+                    }))
+                }
+            }),
+      React.DOM.script({
+                'id': 'initial-data',
+                'type': 'text/plain',
+                'data-json': JSON.stringify(books)
+            })
+        )
+    ));
+}).listen(port);
+
+console.log('Listening on http://localhost:' + port);
